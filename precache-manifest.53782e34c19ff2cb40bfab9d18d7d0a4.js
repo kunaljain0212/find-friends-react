3180efafcf74434e41fdb60b90fb403ec6ef682d
Kunal Jain <kunaljain0212@pop-os.localdomain>
2020-07-13 18:51:43 +0530

    Updates

---
+++ precache-manifest.53782e34c19ff2cb40bfab9d18d7d0a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835e4858ba1506e423cfdc3f2195d639",
+    "url": "/find-friends-react/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/find-friends-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "551a7404fa1434d9b969",
+    "url": "/find-friends-react/static/css/main.65eb13b1.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/find-friends-react/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-friends-react/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551a7404fa1434d9b969",
+    "url": "/find-friends-react/static/js/main.17b90bec.chunk.js"
+  },
+  {
+    "revision": "80ba2c37b82bf78f390b",
+    "url": "/find-friends-react/static/js/runtime-main.8868614f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/find-friends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
